f2b5c75de5e04444dbcc28df83e2c94241296f97
d3ter <tholowy@gmail.com>
2023-04-23 22:36:54 -0500

    [fix] ruta

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,32 @@
+#![feature(proc_macro_hygiene, decl_macro)]
 #[macro_use] extern crate rocket;
+use rocket::http::Header;
+use rocket::{Request, Response};
+use rocket::fairing::{Fairing, Info, Kind};
 
 mod routes;
+
+pub struct CORS;
+
+#[rocket::async_trait]
+impl Fairing for CORS {
+    fn info(&self) -> Info {
+        Info {
+            name: "Attaching CORS headers to responses",
+            kind: Kind::Response
+        }
+    }
+
+    async fn on_response<'r>(&self, _request: &'r Request<'_>, response: &mut Response<'r>) {
+        response.set_header(Header::new("Access-Control-Allow-Origin", "*"));
+        response.set_header(Header::new("Access-Control-Allow-Methods", "POST, GET, PATCH, OPTIONS"));
+        response.set_header(Header::new("Access-Control-Allow-Headers", "*"));
+        response.set_header(Header::new("Access-Control-Allow-Credentials", "true"));
+    }
+}
 
 #[launch]
 fn rocket() ->  _ {
     let routes = routes::routes();
-    rocket::build().mount("/", routes)
+    rocket::build().attach(CORS).mount("/", routes)
 }
--- src/routes.rs
+++ src/routes.rs
@@ -1,27 +1,51 @@
+use rocket::serde::json::Json;
+use serde::{Deserialize, Serialize};
 use rocket::Route;
-
-use std::fs::File;
-use rocket::response::content::RawJson;
-use std::io::Read;
 use rocket::{get, routes};
-
 
 #[get("/")]
 fn index() -> &'static str {
-    "Hello, world!"
+    "Api is running"
 }
 
-#[get("/get")]
-fn get() -> RawJson<String> {
-    let mut file = File::open(format!("static/models/example.json")).unwrap();
-    let mut contents = String::new();
-    file.read_to_string(&mut contents).unwrap(); 
-    RawJson(contents)
+#[derive(Debug, Deserialize, Serialize)]
+struct PetriNet {
+    class: String,
+    nodeKeyProperty: String,
+    nodeDataArray: Vec<NodeData>,
+    linkDataArray: Vec<LinkData>,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+struct NodeData {
+    id: i32,
+    loc: String,
+    text: String,
+    category: String,
+    tokens: Option<i32>,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+struct LinkData {
+    from: i32,
+    to: i32,
+}
+
+#[post("/check_transitions", data = "<petri_net_json>")]
+fn check_transitions(petri_net_json: Json<PetriNet>) -> Json<Vec<String>> {
+    let mut enabled_transitions = Vec::new();
+    let petri_net = petri_net_json.into_inner();
+    for node_data in &petri_net.nodeDataArray {
+        if node_data.category == "transicion" {
+            enabled_transitions.push(node_data.text.clone());
+        }
+    }
+    Json(enabled_transitions)
 }
 
 pub fn routes() -> Vec<Route> {
     routes![
         index,
-        get
+        check_transitions,
     ]
 }
